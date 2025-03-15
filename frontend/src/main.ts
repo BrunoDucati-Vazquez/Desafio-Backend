edabba980571f8ab03533f02b4e02c1e0f4c93e8
BrunoDucati-Vazquez <bducativazquez@gmail.com>
2025-03-15 15:06:58 -0300

    removing frontend

--- frontend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { HttpClientModule } from '@angular/common/http'; // ✅ Import HttpClientModule
-import { provideHttpClient } from '@angular/common/http'; // ✅ Import provideHttpClient
-
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
